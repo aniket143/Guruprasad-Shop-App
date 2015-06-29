3e8f5dc2228623893eb555edfd2eed5fa7854e52
aniket143 <aniket143@gmail.com>
2015-06-29 18:30:44 +0530

    Update index1.js

--- www/js/index1.js
+++ www/js/index1.js
@@ -10,7 +10,7 @@ function startScan() {
 
     cordova.plugins.barcodeScanner.scan(
         function (result) {
-            var s = "Customer Id: <b>" + result.text + "</b><br/>";
+            var s = "<b>Customer Id: " + result.text + "</b><br/>";
             resultDiv.innerHTML = s;
         }, 
         function (error) {
